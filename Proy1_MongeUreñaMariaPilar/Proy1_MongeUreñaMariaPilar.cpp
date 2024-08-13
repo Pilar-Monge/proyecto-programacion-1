0acb1eac8ef277d2365f3ca18ec9032db961e2d0
Pilar Monge Ureña <pilarmongeu@gmail.com>
2024-08-12 20:47:24 -0500

    Hice un primer cambio

--- Proy1_MongeUreñaMariaPilar/Proy1_MongeUreñaMariaPilar.cpp
+++ Proy1_MongeUreñaMariaPilar/Proy1_MongeUreñaMariaPilar.cpp
@@ -2,10 +2,12 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+
+    return 0;
 }
 
 // Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
