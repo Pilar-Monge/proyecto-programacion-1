8b7dbda4aea907e52c73587d70ef6044f0fb3278
Pilar Monge Ureña <pilarmongeu@gmail.com>
2024-09-08 19:02:56 -0500

    Docs:Restore documents

---
+++ Proy1_MongeUreñaMariaPilar/Movie.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <string> 
+#include <cstdio>
+using namespace std;
+
+class Movie {
+
+private:
+	string name;
+	int year;
+	float duration;
+	string country;
+	string synopsis;
+
+public:
+	Movie();
+	Movie(string aName, int aYear, float aDuration, string aCountry, string aSynopsis);
+	void setName(string aName);
+	string  getName();
+	void setYear(int aYear);
+	int  getYear();
+	void setDuration(float aDuration);
+	float getDuration();
+	void setCountry(string aCountry);
+	string getCountry();
+	void setSynopsis( string aSynopsis);
+	string getSynopsis( );
+	void askInformation();
+	void showInformationMovie();
+
+
+};
---
+++ Proy1_MongeUreñaMariaPilar/Proy1_MongeUreñaMariaPilar.cpp
@@ -0,0 +1,127 @@
+// Proy1_MongeUreñaMariaPilar.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include "Movie.h"
+using namespace std;
+
+
+char  userOption = NULL;
+
+void showUserMenu();
+int showUserOption(char userOption);
+
+void showLines() {
+
+	printf("\033[0;32m ");
+	printf("\n ------------------------------------  \n");
+	printf("\033[0m");
+}
+
+void showUserMenu( )
+{
+	showLines();
+	printf(" \t\t Menu  ");
+
+
+
+
+
+	showLines();
+	printf("\n\ta.Archivo  \n");
+	printf("\n\tb.Mantenimiento  \n");
+	printf("\n\tc.Reserva  \n");
+	printf("\n\td.Venta  \n");
+	showLines();
+	printf("\n\t Digite la opcion que quiere : ");
+	
+	scanf_s("%c", &userOption);
+	system("cls");
+	showUserOption(userOption );
+}
+
+
+int showUserOption(char userOption) {
+	
+	switch (userOption)
+	{
+	case 'a':
+		
+		system("cls");
+		printf("\n\t Archivo: \n");
+		showLines();
+		printf("\n\t i.  Acerca de:  \n");
+		printf("\n\t ii. Salir  \n");
+		showLines();
+		printf("\n\t Digite la opcion que quiere: ");
+		////scanf_s();
+	
+		system("pause");
+
+		showUserMenu();
+		break;
+
+	case 'b':
+
+		system("cls");
+		printf("\n\t Mantenimiento: \n");
+		showLines();
+		printf("\n\t i.Peliculas \n");
+		printf("\n\t ii.Salas \n");
+		printf("\n\t iii.Horarios\n");
+		showLines();
+		printf("\n\t Digite la opcion que quiere: ");
+		system("pause");
+
+		showUserMenu( );
+		; break;
+
+	case 'c':
+
+		system("cls");
+
+		printf("\n\t Reserva: ");
+		showLines();
+
+		system("pause");
+	
+		showUserMenu();
+		; break;
+
+	case 'd':
+
+		system("cls");
+
+		printf("\n\t Venta: ");
+		showLines();
+
+		system("pause");
+
+		showUserMenu();
+
+		; break;
+
+	default:
+		printf("\n\n\t Error al digitar la opcion. \n");
+		printf("\n\n\t La opcion que digito es erronea, digitela nuevamente... \n\n");
+
+		showUserMenu();
+		; break;
+	}
+	return 0;
+
+}
+
+
+
+int main()
+{
+	showUserMenu( );
+	
+
+	return 0;
+
+}
